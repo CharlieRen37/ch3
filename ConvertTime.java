10eb3239e12e8de4508e220132f8e163bbaaa863
CharlieRen37 <charlieren2009@gmail.com>
2026-09-17 12:31:45 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class ConvertTime{
 	public static void main(String[] args){
-		final double S_TO_M = 60;
-		final double S_TO_H = 3600;
+		final int S_TO_M = 60;
+		final int S_TO_H = 3600;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Number of Seconds: ");
-		double Sec = in.nextDouble();
+		int Sec = in.nextInt();
 		int Hour = Sec / S_TO_H;
 		int RemH = Sec % S_TO_H;
 		int Min = RemH / S_TO_M;
 		int RemM = RemH % S_TO_M;
-		System.out.print(Hour + Min + RemH);
+		//System.out.print(Hour + " Hours, " + Min + " Minutes, " + RemM + " Seconds");
+		System.out.printf("%d Hours, %d Minutes, %d Seconds", Hour, Min, RemM);
 	}
 }
